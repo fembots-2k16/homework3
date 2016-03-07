1f8b44570aae3c0d31294c09c1ac062c64386add
jake trower <cakeandturtles@gmail.com>
2016-03-07 12:45:38 -0600

    specifying amounts to travel seems to work

--- homework3.py
+++ homework3.py
@@ -5,21 +5,26 @@ import sys
 from geometry_msgs.msg import Twist
 from nav_msgs.msg import Odometry
 
+odom = None
+gps = None
 def processPose(pose_msg):
-    pose = pose_msg.pose.pose.position
+    global odom
+    odom = pose_msg.pose.pose.position
     #print "odom: " + str(pose.x) + ", " + str(pose.y)
 
 def processGps(gps_msg):
+    global gps
     gps = gps_msg.pose.pose.position
     #print "gps: " + str(gps.x) + ", " + str(gps.y)
 
 def main():
-    if len(sys.argv) != 3:
+    if len(sys.argv) != 5:
         print "please run with four command line arguments:"
         print "\t1. number of meters to travel"
         print "\t2. number of degrees to rotate"
         print "\t3. linear speed (meters/second)"
         print "\t4. angular speed (degrees/second)"
+        sys.exit()
     linear_amt = float(sys.argv[1])
     angular_amt = float(sys.argv[2])
     linear_speed = float(sys.argv[3])
@@ -31,18 +36,34 @@ def main():
 
     vel_pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
 
-    time = 0
-
     rospy.init_node('navigator')
     rate = rospy.Rate(10) # 10hz
-    while not rospy.is_shutdown():
-        time += 1
-        twist = Twist() #values default to 0 when new instance initiated
-        twist.linear.x = 1.0
+    linear_amt_traveled = 0
+    angular_amt_traveled = 0
 
+    twist = Twist() #values default to 0 when new instance initiated
+    twist.linear.x = linear_speed
+    twist.angular.z = angular_speed
+
+    for i in range(90):
         #publish the message
-        vel_pub.publish(twist)
-        rate.sleep()
+        if angular_amt_traveled >= angular_amt and linear_amt_traveled >= linear_amt:
+            break
+        else:
+            vel_pub.publish(twist)
+
+        rospy.sleep(0.1) #0.1 seconds?
+
+        linear_amt_traveled += linear_speed*0.1
+        angular_amt_traveled += angular_speed*0.1
+
+    rospy.sleep(3.0)
+    global gps
+    global odom
+    print "finished"
+    print "--------------------------------------_"
+    print "gps: " + str(gps.x) + ", " + str(gps.y)
+    print "odom: " + str(odom.x) + ", " + str(odom.y)
 
 if __name__ == "__main__":
     try:
