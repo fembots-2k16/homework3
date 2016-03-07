a1d20398a93cd65f5bec8a81816800ad39d1b62d
jake trower <cakeandturtles@gmail.com>
2016-03-07 12:18:27 -0600

    rename stagegps.py homework3.py, added cmd line arguments.

--- homework3.py
+++ homework3.py
@@ -1,21 +1,33 @@
 #!/usr/bin/env python
 import rospy
 import math
+import sys
 from geometry_msgs.msg import Twist
 from nav_msgs.msg import Odometry
 
 def processPose(pose_msg):
     pose = pose_msg.pose.pose.position
-    print "odom: " + str(pose.x) + ", " + str(pose.y)
+    #print "odom: " + str(pose.x) + ", " + str(pose.y)
 
 def processGps(gps_msg):
-    linear = gps_msg.twist.linear
-    print "gps: " + str(linear.x) + ", " + str(linear.y)
+    gps = gps_msg.pose.pose.position
+    #print "gps: " + str(gps.x) + ", " + str(gps.y)
 
 def main():
+    if len(sys.argv) != 3:
+        print "please run with four command line arguments:"
+        print "\t1. number of meters to travel"
+        print "\t2. number of degrees to rotate"
+        print "\t3. linear speed (meters/second)"
+        print "\t4. angular speed (degrees/second)"
+    linear_amt = float(sys.argv[1])
+    angular_amt = float(sys.argv[2])
+    linear_speed = float(sys.argv[3])
+    angular_speed = float(sys.argv[4])
+
     ns = "r0"
     pose_sub = rospy.Subscriber('/odom', Odometry, processPose)
-    gps_sub = rospy.Subscriber('/base_pose_ground_truth', Twist, processGps)
+    gps_sub = rospy.Subscriber('/base_pose_ground_truth', Odometry, processGps)
 
     vel_pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
 
